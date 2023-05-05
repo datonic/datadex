a886d710f776bebeb04f178e27a549b56697a794
David Gasquez <davidgasquez@gmail.com>
2023-05-05 10:27:18 +0000

    feat: update packages

--- dbt/models/ethereum/ethereum_logs.sql
+++ dbt/models/ethereum/ethereum_logs.sql
@@ -4,8 +4,6 @@ SET s3_endpoint='ed5d915e0259fcddb2ab1ce5592040c3.r2.cloudflarestorage.com';
 SET s3_access_key_id='43c31ff797ec2387177cabab6d18f15a';
 SET s3_secret_access_key='afb354f05026f2512557922974e9dd2fdb21e5c2f5cbf929b35f0645fb284cf7';
 SET s3_url_style='path';
-SET enable_object_cache=true;
-SET enable_http_metadata_cache=true;
 {% endset %}
 
 {% set results = run_query(r2_setup) %}
--- reports/pages/carbon-dioxide-trend.md
+++ reports/pages/carbon-dioxide-trend.md
@@ -1,7 +1,7 @@
 # Trends in Atmospheric Carbon Dioxide
 
 ```co2_global_trend
-select * from "../../../data/co2_global_trend.parquet"
+select * from "../../../data/climate_co2_global_trend.parquet"
 ```
 
 <LineChart
