2b17b2ced3a6def0ffcad42edcdbe8142136b295
David Gasquez <davidgasquez@gmail.com>
2023-05-30 10:56:34 +0000

    fix: 🩺 fix ethereum models

--- dbt/models/ethereum/ethereum_daily_blocks.sql
+++
@@ -1,5 +0,0 @@
-select date, sum(transaction_count) as tx_count
-from {{ ref("ethereum_blocks") }}
-where date > '2023-01-01'
-group by date
-order by date
--- dbt/models/ethereum/ethereum_logs.sql
+++ dbt/models/ethereum/ethereum_logs.sql
@@ -1,16 +1,22 @@
-{% set r2_setup %}
-SET s3_region="auto";
-SET s3_endpoint='ed5d915e0259fcddb2ab1ce5592040c3.r2.cloudflarestorage.com';
-SET s3_access_key_id='43c31ff797ec2387177cabab6d18f15a';
-SET s3_secret_access_key='afb354f05026f2512557922974e9dd2fdb21e5c2f5cbf929b35f0645fb284cf7';
-SET s3_url_style='path';
-{% endset %}
+{{ config(materialized="view") }}
 
-{% set results = run_query(r2_setup) %}
+with
+    source as (select * from {{ source("ethereum", "logs") }}),
+
+    renamed as (
+        select
+            date,
+            log_index,
+            transaction_hash,
+            transaction_index,
+            address,
+            data,
+            topics,
+            block_timestamp,
+            block_number,
+            block_hash
+        from source
+    )
 
 select *
-from
-    parquet_scan(
-        's3://indexed-xyz/ethereum/decoded/logs/v1.2.0/partition_key=9d/dt=2023/*.parquet',
-        hive_partitioning = 1
-    )
+from renamed
