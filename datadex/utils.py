58376d41e07f98935ca1e97d4ea3080bb9bbf380
David Gasquez <davidgasquez@gmail.com>
2023-11-10 10:52:42 +0100

    chore: 🎨 add tutorial (#35)

--- datadex/utils.py
+++ datadex/utils.py
@@ -1,2 +1,14 @@
+import os
+
+import duckdb
+
+db_dir = os.path.dirname(os.path.abspath(__file__)) + "/../data/"
+
+
 def custom_f():
     return 42
+
+
+def query(sql):
+    with duckdb.connect(database=f"{db_dir}/local.duckdb") as con:
+        return con.sql(sql).df()
