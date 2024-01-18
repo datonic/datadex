41e94f456f6943ed0836a421f0f83f985c9ee752
David Gasquez <davidgasquez@gmail.com>
2024-01-18 15:01:07 +0000

    style: 🗑️ some cleaning

--- datadex/utils.py
+++ datadex/utils.py
@@ -5,10 +5,6 @@ import duckdb
 db_dir = os.path.dirname(os.path.abspath(__file__)) + "/../data/"
 
 
-def custom_f():
-    return 42
-
-
 def query(sql):
     with duckdb.connect(database=f"{db_dir}/local.duckdb") as con:
         return con.sql(sql).df()
