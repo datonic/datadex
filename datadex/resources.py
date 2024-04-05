7c7f221b14c884c8ef9be3291a31f4af49dfeab7
David Gasquez <davidgasquez@gmail.com>
2024-04-05 09:05:55 +0000

    chore: 📌 remove slqfmt [skip ci]

--- datadex/resources.py
+++ datadex/resources.py
@@ -14,9 +14,7 @@ class HuggingFaceResource(ConfigurableResource):
     def upload_dataset(self, dataset, name):
         self.login()
         dataset = Dataset.from_pandas(dataset, split=NamedSplit("main"))
-        r = dataset.push_to_hub(
-            "davidgasquez/" + name, max_shard_size="50000MB"
-        )
+        r = dataset.push_to_hub("davidgasquez/" + name, max_shard_size="50000MB")
         return r
 
 
